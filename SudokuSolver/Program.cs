54219404fbe02d2262c77ef2d58035d39c2fda2d
Rai <robertblake.hardy@outlook.com>
2022-09-05 10:17:41 +0900

    Solve-Algorithm redo

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -12,14 +12,15 @@ public class Program
         PrintBoard(board);
 
         //user inputs known numbers
-        InputNumbers(board);
-
+        // InputNumbers(board);
+        Seed(board);
+        PrintBoard(board);
         //perform the magic
         SolveBoard(board);
 
     }
 
-    public static int[,] NewBoard()
+    private static int[,] NewBoard()
     {
         int[,] board = new int[9, 9];
         for (int i = 0; i < 9; i++)
@@ -33,7 +34,7 @@ public class Program
         return board;
     }
 
-    public static void PrintBoard(int[,] board)
+    private static void PrintBoard(int[,] board)
     {
 
         for (int i = 0; i < 9; i++)
@@ -49,11 +50,11 @@ public class Program
                     Console.Write($"{board[i, j]} ");
                 }
             }
-
         }
+        Console.WriteLine();
     }
 
-    public static void InputNumbers(int[,] board)
+    private static void InputNumbers(int[,] board)
     {
         Console.WriteLine(
             "Please input 1-9. If the number unknown, input 0. Board reads from top to bottom, left to right");
@@ -62,7 +63,7 @@ public class Program
             for (int j = 0; j < 9; j++)
             {
                 bool isConverted = false;
-                string userInput = "";
+                string? userInput = "";
                 do
                 {
                     Console.Write($"What is the number for [{i + 1},{j + 1}]: ");
@@ -89,42 +90,64 @@ public class Program
         Console.WriteLine("Solving Board...");
     }
 
-    public static void SolveBoard(int[,] board)
+    private static void SolveBoard(int[,] board)
     {
+        //row
+        int x = 0;
+        //column
+        int y = 0;
+        //square
+        int z = 0;
+        int iterations = 1;
         int[,] newBoard = board;
-        SolveRow(newBoard);
-        // SolveCol(newBoard);
-        // SolveSquare(newBoard);
-    }
-
-    public static bool SolveRow(int[,] board)
-    {
         int[,] numCounts = CountMatrix();
-        
-        /*
-         Original plan was to solve by individual rows, columns, and squares; however, this will not solve
-         the puzzle, atleast not in a quicker manner. New plan is to check an index's related row, column, and square 
-         to make the best possible choice. 
-         */
-        // for (int i = 0; i < 9; i++)
-        // {
-        //     for (int j = 0; j < 9; j++)
-        //     {
-        //         numCounts[board[i, j]]++;
-        //     }
-        // }
-
-        return false;
+        CountRow(newBoard, numCounts, x);
+        CountCol(newBoard, numCounts, y);
+        CountSquare(newBoard, numCounts, z);
     }
 
-    public static bool SolveCol(int[,] board)
+    private static void CountRow(int[,] board, int[,]numCounts, int row)
     {
-        return false;
+        for (int i = 0; i < 9; i++)
+        {
+            numCounts[board[row, i],0]++;
+        }
     }
 
-    public static bool SolveSquare(int[,] board)
+    public static void CountCol(int[,] board, int[,]numCounts, int col)
     {
-        return false;
+        for (int i = 0; i < 9; i++)
+        {
+            numCounts[board[i, col],1]++;
+        }
+    }
+
+    public static void CountSquare(int[,] board, int[,]numCounts, int square)
+    {
+        //Square's x and y
+        int x = 0;
+        int y = 0;
+        switch (square)
+        {  
+            case 0: x = 0; y = 0; break;
+            case 1: x = 3; y = 0; break;
+            case 2: x = 6; y = 0; break;
+            case 3: x = 0; y = 3; break;
+            case 4: x = 3; y = 3; break;
+            case 5: x = 6; y = 3; break;
+            case 6: x = 0; y = 6; break;
+            case 7: x = 3; y = 6; break;
+            case 8: x = 6; y = 6; break;
+            
+        }
+
+        for (int i = x; i < x + 3; i++)
+        {
+            for (int j = y; j < y + 3; j++)
+            {
+                numCounts[board[i, j],2]++;
+            }
+        }
     }
 
     public static int[,] CountMatrix()
@@ -138,5 +161,33 @@ public class Program
                             {0,0,0},{0,0,0},{0,0,0},
                             {0,0,0} };
         return numCount;
+    }
+
+    public static void Seed(int[,] board)
+    {
+        // Random seed = new Random();
+        //
+        // for (int i = 0; i < 9; i++)
+        // {
+        //     for (int j = 0; j < 9; j++)
+        //     {
+        //         board[i,j] = seed.Next(0, 9);
+        //     }
+        // }
+        
+        //Hard coded seed
+        board[0, 2] = 4; board[0, 6] = 8; board[1, 0] = 7;
+        board[1, 1] = 2; board[1, 3] = 3; board[1, 4] = 5; 
+        board[2, 4] = 4; board[2, 5] = 7; board[2, 8] = 3;
+        board[3, 0] = 4; board[3, 1] = 8; board[3, 5] = 1; 
+        board[3, 6] = 3; board[4, 1] = 6; board[4, 2] = 7;
+        board[4, 6] = 1; board[4, 8] = 2; board[5, 3] = 5;
+        board[5, 8] = 7; board[6, 1] = 7; board[6, 2] = 3;
+        board[6, 3] = 8; board[6, 4] = 1; board[6, 5] = 5;
+        board[6, 7] = 4; board[6, 8] = 9; board[7, 0] = 8; 
+        board[7, 1] = 9; board[7, 2] = 2; board[7, 4] = 7;
+        board[7, 5] = 6; board[7, 7] = 3; board[7, 8] = 1;
+        board[8, 3] = 2; board[8, 7] = 6; board[8, 8] = 8;
+
     }
 }
